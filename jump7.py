d84e79075e9a2b4b4a3539b82bac8187c01f8e16
happy-yan <hhhhhhhh>
2024-08-19 18:59:18 +0800

    [planner]写了一个逢7就跳代码

---
+++ jump7.py
@@ -0,0 +1,12 @@
+a=0 #从1-100跳7的数字
+while a<100:
+    a+=1
+    if a%7==0:#7的倍数
+        pass
+    elif a%10==7:#各位上有7
+        pass
+    elif a//10==7:#十位数上有7
+        pass
+    else:
+        print(a)
+        
